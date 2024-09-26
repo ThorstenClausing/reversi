9320d131264167d90be8e6384ed20d8ab73bc3e5
ThorstenClausing <163404932+ThorstenClausing@users.noreply.github.com>
2024-09-26 12:12:09 +0200

    Create minimax_vs_stochastisch.py
    Erster commit

---
+++ minimax_vs_stochastisch.py
@@ -0,0 +1,11 @@
+from auswertungsumgebung import Auswertungsumgebung
+from spieler import Minimax_Spieler, Stochastischer_Spieler
+from spiellogik import WEISS, SCHWARZ
+
+awu = Auswertungsumgebung()
+spieler_weiss = Minimax_Spieler(WEISS)
+spieler_schwarz = Stochastischer_Spieler(SCHWARZ)
+partie = Partieumgebung(spieler_weiss,spieler_schwarz,awu)
+for _ in range(5):
+  partie.starten()
+print(awu.ergebnis_speicher)
