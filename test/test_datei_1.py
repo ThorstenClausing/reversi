8b9c75ffaee053212281b4d39862369b540147db
ThorstenClausing <163404932+ThorstenClausing@users.noreply.github.com>
2024-09-26 15:11:47 +0200

    Create test_datei_1.py
    Datei verschoben

---
+++ test/test_datei_1.py
@@ -0,0 +1,10 @@
+from spiellogik import GRUNDSTELLUNG, moegliche_zuege
+
+def test_func():
+  zuege = []
+  for zug in moegliche_zuege(GRUNDSTELLUNG):
+    zuege.append(zug[0])
+  assert (2,4) in zuege
+  assert (3,5) in zuege
+  assert (4,2) in zuege
+  assert (5,3) in zuege
