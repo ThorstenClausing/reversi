99e38ebd2102824eb5a744888f48f024b177d7fe
ThorstenClausing <163404932+ThorstenClausing@users.noreply.github.com>
2024-09-26 13:12:52 +0200

    Update minimax_vs_stochastisch.py
    Syntaxkorrektur sys.argv

--- minimax_vs_stochastisch.py
+++ minimax_vs_stochastisch.py
@@ -4,7 +4,7 @@ from spieler import Minimax_Spieler, Stochastischer_Spieler
 from spiellogik import WEISS, SCHWARZ
 from partieumgebung import Partieumgebung
 
-anzahl_partien = int(sys.argv[0])
+anzahl_partien = int(sys.argv[1])
 awu = Auswertungsumgebung()
 spieler_weiss = Minimax_Spieler(WEISS)
 spieler_schwarz = Stochastischer_Spieler(SCHWARZ)
