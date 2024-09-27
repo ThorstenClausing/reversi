7c563c2488b68187e0bb93cbedef8931c2f4735e
ThorstenClausing <163404932+ThorstenClausing@users.noreply.github.com>
2024-09-27 09:59:54 +0200

    Create lernend_vs_stochastisch.py
    Erster commit

---
+++ skripte/lernend_vs_stochastisch.py
@@ -0,0 +1,21 @@
+import sys
+import os
+
+current = os.path.dirname(os.path.realpath(__file__))
+parent = os.path.dirname(current)
+sys.path.append(parent)
+
+from auswertungsumgebung import Auswertungsumgebung
+from spieler import Lernender_Spieler, Stochastischer_Spieler
+from spiellogik import WEISS, SCHWARZ
+from partieumgebung import Partieumgebung
+
+anzahl_partien = int(sys.argv[1])
+awu = Auswertungsumgebung()
+spieler_weiss = Lernender_Spieler(WEISS,awu)
+spieler_schwarz = Stochastischer_Spieler(SCHWARZ)
+partie = Partieumgebung(spieler_weiss,spieler_schwarz,awu)
+for _ in range(anzahl_partien):
+  partie.starten()
+print(awu.ergebnis_speicher)
+awu.bewertung_speichern()
