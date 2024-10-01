6be77eaae9f77d8c40166ef799099aae74bcfec2
ThorstenClausing <163404932+ThorstenClausing@users.noreply.github.com>
2024-10-01 05:07:44 +0200

    Update test_datei_3.py

--- test/test_datei_3.py
+++ test/test_datei_3.py
@@ -2,6 +2,7 @@ from spiellogik_alternativ import moegliche_zuege as mz_neu
 from spiellogik_alternativ import zug_spielen as zs_neu
 from spiellogik import moegliche_zuege as mz_alt
 from spiellogik import zug_spielen as zs_alt
+from spiellogik import SCHWARZ
 import numpy as np
 
 stellung = np.zeros([8,8])
@@ -9,5 +10,7 @@ stellung[2,4] = stellung[3,3] = stellung[3,4] = stellung[4,4] = 1
 stellung[4,3] = -1
 
 def test_func():
-  #assert (mz_neu(stellung) == mz_alt(stellung)).all()
+  for i in range(len(mz_neu(stellung)):
+    assert mz_neu(stellung)[i][0] == mz_alt(stellung)[i][0]
+    assert (mz_neu(stellung)[i][1] == mz_alt(stellung)[i][1]).all()
   assert (zs_neu(stellung, mz_neu(stellung)[0],SCHWARZ) == zs_alt(stllung, mz_alt(stellung)[0],SCHWARZ)).all()
