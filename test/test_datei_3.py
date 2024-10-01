9a193f548a172cfffd70f71793231e858a565f06
ThorstenClausing <163404932+ThorstenClausing@users.noreply.github.com>
2024-10-01 04:55:45 +0200

    Update test_datei_3.py
    Importe korrigiert

--- test/test_datei_3.py
+++ test/test_datei_3.py
@@ -1,5 +1,7 @@
-from spiellogik_alternativ import moegliche_zuege, zug_spielen as mz_neu, zs_neu
-from spiellogik import moegliche_zuege, zug_spielen as mz_alt, zs_alt
+from spiellogik_alternativ import moegliche_zuege as mz_neu
+from spiellogik_alternativ import zug_spielen as zs_neu
+from spiellogik import moegliche_zuege as mz_alt
+from spiellogik import zug_spielen as zs_alt
 import numpy as np
 
 stellung = np.zeros([8,8])
