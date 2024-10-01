043a6040f6720257c7b3e0a87ac1710bd82c5a22
ThorstenClausing <163404932+ThorstenClausing@users.noreply.github.com>
2024-10-01 05:27:59 +0200

    Update test_datei_3.py

--- test/test_datei_3.py
+++ test/test_datei_3.py
@@ -10,8 +10,10 @@ stellung[2,4] = stellung[3,3] = stellung[3,4] = stellung[4,4] = 1
 stellung[4,3] = -1
 
 def test_func():
+  assert len(mz_neu(stellung)) == len(mz_alt(stellung))
   for i in range(len(mz_neu(stellung))):
     assert mz_neu(stellung)[i][0] == mz_alt(stellung)[i][0]
+    assert len(mz_neu(stellung)[i][1]) == len(mz_alt(stellung)[i][1])
     for j in range(len(mz_neu(stellung)[i][1])):
       assert (mz_neu(stellung)[i][1][j] == mz_alt(stellung)[i][1][j]).all()
-  assert (zs_neu(stellung, mz_neu(stellung)[0],SCHWARZ) == zs_alt(stellung, mz_alt(stellung)[0],SCHWARZ)).all()
+  assert (zs_neu(stellung, mz_neu(stellung)[-1],SCHWARZ) == zs_alt(stellung, mz_alt(stellung)[-1],SCHWARZ)).all()
