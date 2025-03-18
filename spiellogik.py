fdde1cf8b9d47803be2a546fedd7f13c44bb0f02
ThorstenClausing <163404932+ThorstenClausing@users.noreply.github.com>
2025-03-18 12:02:10 +0800

    Update spiellogik.py
    Ersetzt schwarz/weiß durch AM_ZUG/NICHT_AM_ZUG

--- spiellogik.py
+++ spiellogik.py
@@ -1,7 +1,8 @@
 import numpy as np
 
-WEISS = 1
-SCHWARZ = -1
+AM_ZUG = 1
+NICHT_AM_ZUG = -1
+LEER = 0
 
 OBEN_LINKS = np.array([-1, -1])
 OBEN = np.array([-1, 0])
@@ -24,67 +25,68 @@ class Stellung(np.ndarray):
     def __array_wrap__(self, array, context=None, return_scalar=False):
         return array[()]
       
-    def schwarz(self, liste):
+    def __nicht_am_zug(self, liste):
         for index in liste:
-            self[index[0],index[1]] = -1
+            self[index[0],index[1]] = NICHT_AM_ZUG
 
-    def weiss(self, liste):
+    def __am_zug(self, liste):
         for index in liste:
-            self[index[0],index[1]] = 1  
+            self[index[0],index[1]] = AM_ZUG  
 
     def grundstellung(self):
-        self.weiss([(3,3),(4,4)])
-        seld.schwarz([(3,4),(4,3)])
+        self.__nicht_am_zug([(3,3),(4,4)])
+        seld.__am_zug([(3,4),(4,3)])
 
-    def moegliche_zuege(self, farbe):
+    def moegliche_zuege(self):
         """
-        Finds all possible moves for the given color.
+        Finds all possible moves for the player who is to move.
         """
         zuege = []
         for z in range(8):
             for s in range(8):
-                if self[z, s] == 0:
+                if self[z, s] == LEER:
                     r_liste = []
                     for richtung in RICHTUNGEN:
                         a, b = np.array([z, s]) + richtung
-                        if 0 <= a < 8 and 0 <= b < 8 and self[a, b] == -1 * farbe:
-                            if self.__einschluss(a, b, richtung, farbe):
+                        if 0 <= a < 8 and 0 <= b < 8 and self[a, b] == NICHT_AM_ZUG:
+                            if self.__einschluss(a, b, richtung):
                                 r_liste.append(richtung)
                     if r_liste:
                         zuege.append(((z, s), r_liste))
         return zuege
 
-    def __einschluss(self, z, s, richtung, farbe):
+    def __einschluss(self, z, s, richtung):
         """
         Checks if a move is valid by checking for flanking pieces of opposite color.
         """
-        if not (0 <= z < 8 and 0 <= s < 8) or self[z, s] != -1 * farbe:
+        if not (0 <= z < 8 and 0 <= s < 8) or self[z, s] != NICHT_AM_ZUG:
             assert False
         a, b = z, s
         while True:
             c, d = np.array([a, b]) + richtung
             if not (0 <= c < 8 and 0 <= d < 8):
                 return False
-            if self[c, d] == farbe:
+            if self[c, d] == AM_ZUG:
                 return True
-            if self[c, d] == 0:
+            if self[c, d] == LEER:
                 return False
             a, b = c, d
 
-    def zug_spielen(self, zug, farbe):
+    def zug_spielen(self, zug):
         """
         Applies a move to the board, flipping captured pieces.
         """
         z, s = zug[0]
-        assert self[z, s] == 0
+        assert self[z, s] == LEER
         neue_stellung = stellung.copy()
-        self[z, s] = farbe
+        self[z, s] = AM_ZUG
         for richtung in zug[1]:
-            umzudrehende_steine = self.__eingeschlossene_steine(z, s, richtung, farbe)
+            umzudrehende_steine = self.__eingeschlossene_steine(z, s, richtung)
             for stein in umzudrehende_steine:
-                self[stein[0], stein[1]] = farbe
+                self[stein[0], stein[1]] = AM_ZUG
+        self.__gegenspieler_kommt_zum_zug()
        
-    def __eingeschlossene_steine(self, z, s, richtung, farbe):
+    def __eingeschlossene_steine(self, z, s, richtung):
         """
         Finds all pieces that would be flipped by a move.
         """
@@ -95,9 +97,15 @@ class Stellung(np.ndarray):
             c, d = np.array([a, b]) + richtung
             if not (0 <= c < 8 and 0 <= d < 8):
                 assert False
-            if self[c, d] == farbe:
+            if self[c, d] == AM_ZUG:
                 return steine
-            if self[c, d] == 0:
+            if self[c, d] == LEER:
                 assert False
             steine.append((c, d))
             a, b = c, d
+
+    def __gegenspieler_kommt_zum_zug(self):
+        """
+        Vertauscht AM_ZUG und NICHT_AM_ZUG.
+        """
+        self = np.negative(self)
