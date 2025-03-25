8f89fca87e489abcf50ba3402970242e2567d00e
ThorstenClausing <163404932+ThorstenClausing@users.noreply.github.com>
2025-03-25 10:16:21 +0800

    Update optimierend_vs_stochastisch.py

--- skripte/optimierend_vs_stochastisch.py
+++ skripte/optimierend_vs_stochastisch.py
@@ -5,18 +5,17 @@ current = os.path.dirname(os.path.realpath(__file__))
 parent = os.path.dirname(current)
 sys.path.append(parent)
 
-from auswertungsumgebung import Auswertungsumgebung
-from spieler import Optimierender_Spieler, Stochastischer_Spieler
-from spiellogik import WEISS, SCHWARZ
+from auswertungsumgebung import Ergebnisspeicher
+from spieler import Optimierender_Spieler
 from partieumgebung import Partieumgebung
 
 anzahl_partien = int(sys.argv[1])
-awu = Auswertungsumgebung()
-awu.bewertung_laden('bwt_l_vs_s_10_000.of')
-spieler_weiss = Optimierender_Spieler(WEISS,awu)
-spieler_schwarz = Stochastischer_Spieler(SCHWARZ)
+speicher = Ergebnisspeicher(True, True)
+spieler_schwarz = Optimierender_Spieler(speicher, 5)
+spieler_weiss = Optimierender_Spieler(speicher, 5)
+
 partie = Partieumgebung(spieler_weiss,spieler_schwarz,awu)
 for _ in range(anzahl_partien):
   partie.starten()
-print(awu.ergebnis_speicher)
-awu.bewertung_speichern('neu.of')
+speicher.bewertung_drucken()
+speicher.bewertung_speichern('neu.of')
