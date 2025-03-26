ae40884e18e84e3914a1144450e0c09fd9158f3a
ThorstenClausing <163404932+ThorstenClausing@users.noreply.github.com>
2025-03-26 18:59:03 +0800

    Update optimierend_vs_stochastisch.py

--- skripte/optimierend_vs_stochastisch.py
+++ skripte/optimierend_vs_stochastisch.py
@@ -12,26 +12,26 @@ from partieumgebung import Partieumgebung
 anzahl_partien = int(sys.argv[1])
 anzahl_tests = int(sys.argv[2])
 speicher = Ergebnisspeicher(True, True)
-spieler_schwarz = Lernender_Spieler(speicher, 2)
-spieler_weiss = Lernender_Spieler(speicher, 2)
+spieler_schwarz = Lernender_Spieler(speicher)
+spieler_weiss = Lernender_Spieler(speicher)
 spieler_opt = Optimierender_Spieler(speicher)
 spieler_stoch = Stochastischer_Spieler()
 partie = Partieumgebung(spieler_schwarz, spieler_weiss, speicher)
 test_schwarz = Partieumgebung(spieler_opt, spieler_stoch, speicher)
 test_weiss = Partieumgebung(spieler_stoch, spieler_opt, speicher)
 
-for y in [2, 3, 4, 5]
-spieler_schwarz.epsilonkehrwert_eingeben(y)
-spieler_weiss.epsiolonkehrwert_eingeben(y)
-    for _ in range(anzahl_partien):
-        partie.partie_starten()
-    test_schwarz.testprotokoll_zuruecksetzen()
-    for _ in range(anzahl_tests):
-        test_schwarz.test_starten()
-    test_schwarz.testprotokoll_drucken()
-    test_weiss.testprotokoll_zuruecksetzen()
-    for _ in range(anzahl_tests):
-        test_weiss.test_starten()
-    test_weiss.testprotokoll_drucken()
+for y in [2, 3, 4, 5]:
+    spieler_schwarz.epsilonkehrwert_eingeben(y)
+    spieler_weiss.epsilonkehrwert_eingeben(y)
+        for _ in range(anzahl_partien):
+            partie.partie_starten()
+        test_schwarz.testprotokoll_zuruecksetzen()
+        for _ in range(anzahl_tests):
+            test_schwarz.test_starten()
+        test_schwarz.testprotokoll_drucken()
+        test_weiss.testprotokoll_zuruecksetzen()
+        for _ in range(anzahl_tests):
+            test_weiss.test_starten()
+        test_weiss.testprotokoll_drucken()
 
 speicher.bewertung_speichern('neu.of')
