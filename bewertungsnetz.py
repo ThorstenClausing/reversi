9f84bee496534c7a7629999c47b4f0ff54dbc407
ThorstenClausing <thorsten.clausing@studium.fernuni-hagen.de>
2025-04-16 23:03:46 +0800

    Update

--- bewertungsnetz.py
+++ bewertungsnetz.py
@@ -3,17 +3,24 @@ from torch.utils.data import Dataset
 
 class Bewertungsnetz(nn.Module):
     def __init__(self):
-        super().__init__()
-        self.flatten = nn.Flatten()
-        self.linear_relu_stack = nn.Sequential(
-            nn.Linear(64, 34),
-            nn.ReLU(),
-            nn.Linear(34, 1),
-        )
+        super(Bewertungsnetz, self).__init__()
+        self.innere_schicht_eins = nn.Linear(64, 96)
+        self.innere_schicht_zwei = nn.Linear(96, 34)
+        self.ausgabeschicht = nn.Linear(34, 1)
+        self.aktivierung = nn.Tanh()
+        nn.init.xavier_uniform_(self.innere_schicht_eins.weight)
+        nn.init.xavier_uniform_(self.innere_schicht_zwei.weight)
+        nn.init.xavier_uniform_(self.ausgabeschicht.weight)
+        nn.init.zeros_(self.innere_schicht_eins.bias)
+        nn.init.zeros_(self.innere_schicht_zwei.bias)
+        nn.init.zeros_(self.ausgabeschicht.bias)
 
     def forward(self, x):
-        x = self.flatten(x)
-        bewertung = self.linear_relu_stack(x)
+        z = self.innere_schicht_eins(x)
+        z = self.aktivierung(z)
+        z = self.innere_schicht_zwei(z)
+        z = self.aktivierung(z)
+        bewertung = self.ausgabeschicht(z)
         return bewertung
 
 class Bewertungsdaten(Dataset):
@@ -25,3 +32,30 @@ class Bewertungsdaten(Dataset):
 
     def __getitem__(self, idx):
         return self.liste[idx][0], self.liste[idx][1]
+
+class Faltendes_Bewertungsnetz(nn.Module):
+    def __init__(self):
+        super(Faltendes_Bewertungsnetz, self).__init__()
+        self.innere_schicht_eins = nn.Conv2d(3, 9, kernel_size=3, padding=1, groups=3)
+        self.innere_schicht_zwei = nn.Conv2d(9, 9, kernel_size=5, padding=2, groups=3)
+        self.innere_schicht_drei = nn.Linear(576, 300)
+        self.ausgabeschicht = nn.Linear(300, 1)
+        self.aktivierung = nn.Tanh()
+        self.flatten = nn.Flatten()
+        nn.init.xavier_uniform_(self.innere_schicht_eins.weight)
+        nn.init.xavier_uniform_(self.innere_schicht_zwei.weight)
+        nn.init.xavier_uniform_(self.innere_schicht_drei.weight)
+        nn.init.xavier_uniform_(self.ausgabeschicht.weight)
+        nn.init.zeros_(self.innere_schicht_eins.bias)
+        nn.init.zeros_(self.innere_schicht_zwei.bias)
+        nn.init.zeros_(self.innere_schicht_drei.bias)
+        nn.init.zeros_(self.ausgabeschicht.bias)
+
+    def forward(self, x):
+        z = self.innere_schicht_eins(x)
+        z = self.innere_schicht_zwei(z)
+        z = self.flatten(z)
+        z = self.innere_schicht_drei(z)
+        z = self.aktivierung(z)
+        bewertung = self.ausgabeschicht(z)
+        return bewertung
--- skripte/netztraining.py
+++ skripte/netztraining.py
@@ -12,14 +12,14 @@ import pickle
 import random
 import numpy as np
 from bewertungsnetz import Bewertungsdaten, Bewertungsnetz
-from spiellogik import Stellung
+#from spiellogik import Stellung
 
 training_liste = []
 test_liste = []
 zaehler = 0
 bewertungen = {}
 durchschnitt = 0
-dateiliste = ['../reversi' + str(i) + '.of' for i in [0, 34, 89, 124]]
+dateiliste = ['../reversi' + str(i) + '.of' for i in [0, 1, 2]]
 
 for datei in dateiliste:
     with (open(datei,'rb')) as f:
@@ -29,7 +29,7 @@ for datei in dateiliste:
         bewertung = bewertungen[stellung_bytes][0]/bewertungen[stellung_bytes][1]
         eingabe = (torch.from_numpy(stellung)).to(torch.float32)
         ausgabe = (torch.tensor([bewertung, ])).to(torch.float32)
-        if not zaehler % 2:
+        if zaehler % 3:
             training_liste.append((eingabe, ausgabe))
         else:
             test_liste.append((eingabe, ausgabe))
@@ -45,16 +45,14 @@ for _, bewertung in test_liste:
 random.shuffle(training_liste)
 training_daten = Bewertungsdaten(training_liste)
 test_daten = Bewertungsdaten(test_liste)
-training_datengeber = DataLoader(training_daten, batch_size=64, shuffle=True)
-test_datengeber = DataLoader(test_daten, batch_size=64, shuffle=True)
+training_datengeber = DataLoader(training_daten, batch_size=32, shuffle=True)
+test_datengeber = DataLoader(test_daten, batch_size=32, shuffle=True)
 
 modell = Bewertungsnetz()
 
 def train_loop(datengeber, modell, verlustfunktion, optimizer):
     size = len(datengeber.dataset)
-    # Set the model to training mode - important for batch normalization and dropout layers
-    # Unnecessary in this situation but added for best practices
-    modell.train()
+    modell.train()      # Unnecessary in this situation but added for best practices
     for batch, (X, y) in enumerate(datengeber):
         # Compute prediction and loss
         vorhersage = modell(X)
@@ -66,19 +64,14 @@ def train_loop(datengeber, modell, verlustfunktion, optimizer):
         optimizer.zero_grad()
 
         if batch % 100 == 0:
-            verlust, current = verlust.item(), batch * 64 + len(X)
-            print(f"loss: {verlust:>7f}  [{current:>5d}/{size:>5d}]")
+            verlust, current = verlust.item(), batch * 32 + len(X)
+            print(f"Verlust: {verlust:>7f}  [{current:>5d}/{size:>5d}]")
 
 
 def test_loop(datengeber, modell, verlustfunktion):
-    # Set the model to evaluation mode - important for batch normalization and dropout layers
-    # Unnecessary in this situation but added for best practices
-    modell.eval()
+    modell.eval()       # Unnecessary in this situation but added for best practices
     num_batches = len(datengeber)
     test_loss, r_zaehler = 0, 0
-
-    # Evaluating the model with torch.no_grad() ensures that no gradients are computed during test mode
-    # also serves to reduce unnecessary gradient computations and memory usage for tensors with requires_grad=True
     with torch.no_grad():
         for X, y in datengeber:
             vorhersage = modell(X)
@@ -91,8 +84,8 @@ def test_loop(datengeber, modell, verlustfunktion):
     r_quadrat = 1 - r_zaehler/r_nenner
     print(f"Testergebnis\n Durchschnittsverlust: {(test_loss):>8f}, R_quadrat: {r_quadrat} \n")
 
-optimierer = torch.optim.RMSprop(modell.parameters())
-epochen = 25
+optimierer = torch.optim.Adam(modell.parameters(), lr=0.001)
+epochen = 20
 for t in range(epochen):
     print(f"Epoche {t+1}\n-------------------------------")
     train_loop(training_datengeber, modell, nn.MSELoss(), optimierer)
