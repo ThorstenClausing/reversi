03c4970ca064f1b21baca5e70ed030d8f10dda83
ThorstenClausing <163404932+ThorstenClausing@users.noreply.github.com>
2025-09-29 11:21:56 +0200

    Update testskript_minimax_faltendes_netz.py
    Gewichte V2

--- skripte/testskript_minimax_faltendes_netz.py
+++ skripte/testskript_minimax_faltendes_netz.py
@@ -20,7 +20,7 @@ from partieumgebung import Partieumgebung
 anzahl_tests = 1000
 speicher = Faltendes_Bewertungsnetz()
 
-speicher.load_state_dict(torch.load("Gewichte/faltende_gewichte_v1_"))
+speicher.load_state_dict(torch.load("Gewichte/faltende_gewichte_v2"))
 print('Gewichte geladen.')
 
 spieler_opt = Optimierender_Spieler(speicher)
@@ -31,7 +31,7 @@ test_schwarz = Partieumgebung(spieler_opt, spieler_stoch)
 test_schwarz.testprotokoll_zuruecksetzen()
 for _ in range(anzahl_tests):
     test_schwarz.test_starten()
-print("Test schwarz:")
+print("Test schwarz (V2):")
 test_schwarz.testprotokoll_drucken()
 #test_weiss.testprotokoll_zuruecksetzen()
 #for _ in range(anzahl_tests):
