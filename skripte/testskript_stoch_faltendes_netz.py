9d338b6d9775b4fe87e7f2a7fa7b163f31072446
ThorstenClausing <163404932+ThorstenClausing@users.noreply.github.com>
2025-10-03 13:32:55 +0200

    Update testskript_stoch_faltendes_netz.py

--- skripte/testskript_stoch_faltendes_netz.py
+++ skripte/testskript_stoch_faltendes_netz.py
@@ -18,23 +18,25 @@ from spieler import Optimierender_Spieler, Stochastischer_Spieler
 from partieumgebung import Partieumgebung
 
 anzahl_tests = 1000
-speicher = Faltendes_Bewertungsnetz()
 
-speicher.load_state_dict(torch.load("Gewichte/faltende_gewichte_v1_", weights_only=True))
-print('Gewichte geladen.')
+for variante in ["v1_", "v2", "schwarz", "weiss"]:
+    speicher = Faltendes_Bewertungsnetz()
+    datei = "Gewichte/faltende_gewichte_" + variante
+    speicher.load_state_dict(torch.load(datei, weights_only=True))
+    print('Faltende Gewichte ', variante, ' geladen.')
 
-spieler_opt = Optimierender_Spieler(speicher)
-spieler_stoch = Stochastischer_Spieler()
-test_schwarz = Partieumgebung(spieler_opt, spieler_stoch)
-test_weiss = Partieumgebung(spieler_stoch, spieler_opt)
+    spieler_opt = Optimierender_Spieler(speicher)
+    spieler_stoch = Stochastischer_Spieler()
+    test_schwarz = Partieumgebung(spieler_opt, spieler_stoch)
+    test_weiss = Partieumgebung(spieler_stoch, spieler_opt)
 
-test_schwarz.testprotokoll_zuruecksetzen()
-for _ in range(anzahl_tests):
-    test_schwarz.test_starten()
-print("Test schwarz (V1[k]):")
-test_schwarz.testprotokoll_drucken()
-test_weiss.testprotokoll_zuruecksetzen()
-for _ in range(anzahl_tests):
-    test_weiss.test_starten()
-print("Test weiß (V1[k]):")
-test_weiss.testprotokoll_drucken()
+    test_schwarz.testprotokoll_zuruecksetzen()
+    for _ in range(anzahl_tests):
+        test_schwarz.test_starten()
+    print("Test schwarz (", variante, "[k]):")
+    test_schwarz.testprotokoll_drucken()
+    test_weiss.testprotokoll_zuruecksetzen()
+    for _ in range(anzahl_tests):
+        test_weiss.test_starten()
+    print("Test weiß (", variante, "[k]):")
+    test_weiss.testprotokoll_drucken()
