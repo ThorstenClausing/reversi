8610b6aea7304c04077461cef2712ecbd6d7afe5
ThorstenClausing <thorsten.clausing@studium.fernuni-hagen.de>
2025-10-03 12:41:38 +0200

    Neue Testskripte

---
+++ skripte/testskript_stoch_faltendes_netz.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 29 15:09:11 2025
+
+@author: Thorsten
+
+"""
+import sys
+import os
+
+current = os.path.dirname(os.path.realpath(__file__))
+parent = os.path.dirname(current)
+sys.path.append(parent)
+
+import torch
+from bewertungsnetz import Faltendes_Bewertungsnetz
+from spieler import Optimierender_Spieler, Stochastischer_Spieler
+from partieumgebung import Partieumgebung
+
+anzahl_tests = 1000
+speicher = Faltendes_Bewertungsnetz()
+
+speicher.load_state_dict(torch.load("../Gewichte/faltende_gewichte_weiss", weights_only=True))
+print('Gewichte geladen.')
+
+spieler_opt = Optimierender_Spieler(speicher)
+spieler_stoch = Stochastischer_Spieler()
+test_schwarz = Partieumgebung(spieler_opt, spieler_stoch)
+test_weiss = Partieumgebung(spieler_stoch, spieler_opt)
+
+test_schwarz.testprotokoll_zuruecksetzen()
+for _ in range(anzahl_tests):
+    test_schwarz.test_starten()
+print("Test schwarz:")
+test_schwarz.testprotokoll_drucken()
+test_weiss.testprotokoll_zuruecksetzen()
+for _ in range(anzahl_tests):
+    test_weiss.test_starten()
+print("Test weiß:")
+test_weiss.testprotokoll_drucken()
