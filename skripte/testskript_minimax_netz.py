3816253c5da7066db7826aa981cf9bf2e0225f00
ThorstenClausing <163404932+ThorstenClausing@users.noreply.github.com>
2025-10-03 09:10:06 +0200

    Update testskript_minimax_netz.py

--- skripte/testskript_minimax_netz.py
+++ skripte/testskript_minimax_netz.py
@@ -20,7 +20,7 @@ from partieumgebung import Partieumgebung
 anzahl_tests = 1000
 speicher = Bewertungsnetz()
 
-speicher.load_state_dict(torch.load("Gewichte/gewichte_schwarz", weights_only=True))
+speicher.load_state_dict(torch.load("Gewichte/gewichte_v2", weights_only=True))
 print('Gewichte geladen.')
 
 spieler_opt = Optimierender_Spieler(speicher)
@@ -31,7 +31,7 @@ test_schwarz = Partieumgebung(spieler_opt, spieler_stoch)
 test_schwarz.testprotokoll_zuruecksetzen()
 for _ in range(anzahl_tests):
     test_schwarz.test_starten()
-print("Test schwarz (Schwarz[kanonisch]):")
+print("Test schwarz (V2[kanonisch]):")
 test_schwarz.testprotokoll_drucken()
 #test_weiss.testprotokoll_zuruecksetzen()
 #for _ in range(anzahl_tests):
