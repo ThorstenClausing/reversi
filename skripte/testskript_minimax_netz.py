d1505c694cfbc59109e63d8428be558fb84982ff
ThorstenClausing <163404932+ThorstenClausing@users.noreply.github.com>
2025-10-16 09:06:29 +0200

    Update testskript_minimax_netz.py

--- skripte/testskript_minimax_netz.py
+++ skripte/testskript_minimax_netz.py
@@ -17,7 +17,7 @@ from bewertungsnetz import Bewertungsnetz
 from spieler import Optimierender_Spieler, Alpha_Beta_Spieler
 from partieumgebung import Partieumgebung
 
-anzahl_tests = 100
+anzahl_tests = 200
 speicher = Bewertungsnetz()
 
 speicher.load_state_dict(torch.load("Gewichte/gewichte_v2", weights_only=True))
