b963577396beb970c2eb09c22adc8751a2b40154
ThorstenClausing <163404932+ThorstenClausing@users.noreply.github.com>
2025-10-20 11:26:13 +0200

    Change variant from 'weiss' to 'schwarz'

--- skripte/testskript_minimax_netz.py
+++ skripte/testskript_minimax_netz.py
@@ -20,7 +20,7 @@ from partieumgebung import Partieumgebung
 anzahl_tests = 200
 speicher = Bewertungsnetz()
 
-variante = "weiss" # Auswahl: v1_, v2, schwarz, weiss
+variante = "schwarz" # Auswahl: v1_, v2, schwarz, weiss
 speicher.load_state_dict(torch.load("Gewichte/gewichte_" + variante, weights_only=True))
 print('Gewichte geladen.')
 
