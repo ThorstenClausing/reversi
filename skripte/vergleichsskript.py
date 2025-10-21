8623b31b96e3bcb98d8a4781d6dbe7fececd376a
ThorstenClausing <thorsten.clausing@studium.fernuni-hagen.de>
2025-10-21 20:25:16 +0200

    Effizienteres Vergleichsskript

--- skripte/vergleichsskript.py
+++ skripte/vergleichsskript.py
@@ -1,15 +1,17 @@
-import sys
-import os
+#import sys
+#import os
 
-current = os.path.dirname(os.path.realpath(__file__))
-parent = os.path.dirname(current)
-sys.path.append(parent)
+#current = os.path.dirname(os.path.realpath(__file__))
+#parent = os.path.dirname(current)
+#sys.path.append(parent)
 
 import pickle
 import zipfile
-import gc
 
-liste = []
+print("Start")
+
+liste_eins = []
+liste_zwei = []
 anzahl_doppelte = 0
 
 with zipfile.ZipFile("../daten/reversi_schwarz.zip", mode="r") as archiv:
@@ -17,15 +19,49 @@ with zipfile.ZipFile("../daten/reversi_schwarz.zip", mode="r") as archiv:
         with archiv.open(datei, mode="r") as datei_av:
             bewertungen = pickle.load(datei_av)
             for tupel in bewertungen:
-                liste.append(tupel[0])
+                liste_eins.append(tupel[0])
+                
+print("Schwarz geladen")
+liste_eins.sort(reverse=True)
+print("Schwarz sortiert")
 
 with zipfile.ZipFile("../daten/reversi_weiss.zip", mode="r") as archiv:
     for datei in archiv.namelist():
         with archiv.open(datei, mode="r") as datei_av:
             bewertungen = pickle.load(datei_av)
             for tupel in bewertungen:
-                if tupel[0] in liste:
-                    anzahl_doppelte += 1
-                    print(tupel[0])
+                liste_zwei.append(tupel[0])
+                
+print("Weiß geladen")
+liste_zwei.sort(reverse=True)
+print("Weiß sortiert")
+
+eins = liste_eins.pop()
+zwei = liste_zwei.pop()
+fertig = False
+
+while not fertig:
+    while eins < zwei:
+        if not liste_eins:
+            fertig = True
+            break
+        else:
+            eins = liste_eins.pop()
+    while eins > zwei:
+        if not liste_zwei:
+            fertig = True
+            break
+        else:
+            zwei = liste_zwei.pop()    
+    while eins == zwei:
+        anzahl_doppelte += 1
+        if not (liste_eins and liste_zwei):
+            fertig = True
+            break
+        else:
+            eins = liste_eins.pop()
+            zwei = liste_zwei.pop()
+        
 
 print("Anzahl doppelte Stellungen:", anzahl_doppelte)
+print("Ende")
