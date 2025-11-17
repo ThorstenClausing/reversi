b5fa7a8c79ce1c1c4225a30e8f9d278fd4404287
ThorstenClausing <163404932+ThorstenClausing@users.noreply.github.com>
2025-11-17 14:17:59 +0100

    Adjust test count and update weight loading path
    Reduced the number of tests from 1,000 to 10 and updated the path for loading weights. Commented out the white player test section.

--- skripte/testskript_minimax_netz.py
+++ skripte/testskript_minimax_netz.py
@@ -17,11 +17,11 @@ from bewertungsnetz import Bewertungsnetz
 from spieler import Optimierender_Spieler, Alpha_Beta_Spieler
 from partieumgebung import Partieumgebung
 
-anzahl_tests = 1_000
+anzahl_tests = 10
 speicher = Bewertungsnetz()
 
 #variante = "v2" # Auswahl: v1_, v2, schwarz, weiss
-speicher.load_state_dict(torch.load("../Gewichte/tiefe_gewichte_sigma", weights_only=True))
+speicher.load_state_dict(torch.load("Gewichte/tiefe_gewichte_sigma", weights_only=True))
 print('Gewichte geladen.')
 
 spieler_opt = Optimierender_Spieler(speicher)
@@ -29,15 +29,15 @@ tiefe = 6
 spieler_minimax = Alpha_Beta_Spieler(tiefe)
 print("Alpha-Beta-Tiefe ", tiefe, sep='')
 test_schwarz = Partieumgebung(spieler_opt, spieler_minimax)
-test_weiss = Partieumgebung(spieler_minimax, spieler_opt)
+#test_weiss = Partieumgebung(spieler_minimax, spieler_opt)
 
 test_schwarz.testprotokoll_zuruecksetzen()
 for _ in range(anzahl_tests):
     test_schwarz.test_starten()
 print("Test schwarz (tief sigma):")
 test_schwarz.testprotokoll_drucken()
-test_weiss.testprotokoll_zuruecksetzen()
-for _ in range(anzahl_tests):
-    test_weiss.test_starten()
-print("Test weiß (tief sigma):")
-test_weiss.testprotokoll_drucken()
+#test_weiss.testprotokoll_zuruecksetzen()
+#for _ in range(anzahl_tests):
+#    test_weiss.test_starten()
+#print("Test weiß (tief sigma):")
+#test_weiss.testprotokoll_drucken()
