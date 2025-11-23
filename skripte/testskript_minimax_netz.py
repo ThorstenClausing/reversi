9a8d52ef8ff371667a5e58dda1386d5f5f431c64
ThorstenClausing <163404932+ThorstenClausing@users.noreply.github.com>
2025-11-23 15:00:21 +0100

    Update weights file path in test script

--- skripte/testskript_minimax_netz.py
+++ skripte/testskript_minimax_netz.py
@@ -20,7 +20,7 @@ from partieumgebung import Partieumgebung
 anzahl_tests = 100
 speicher = Bewertungsnetz()
 
-gewichte = "Gewichte/tiefe_gewichte_sigsig_mehrfach_final"
+gewichte = "Gewichte/tiefe_gewichte_sigsig_mehrfach_weiss"
 speicher.load_state_dict(torch.load(gewichte, weights_only=True))
 print('Gewichte geladen.', gewichte)
 
