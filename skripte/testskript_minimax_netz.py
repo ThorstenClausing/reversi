fef4dd2e98b9ccb8abdec1173c6aa92c803e3066
ThorstenClausing <163404932+ThorstenClausing@users.noreply.github.com>
2025-11-27 09:52:10 +0100

    Update testskript_minimax_netz.py

--- skripte/testskript_minimax_netz.py
+++ skripte/testskript_minimax_netz.py
@@ -21,7 +21,7 @@ from partieumgebung import Partieumgebung
 anzahl_tests = 100
 speicher = Bewertungsnetz()
 
-gewichte = "Gewichte/tiefe_gewichte_sigsig_mehrfach_160"
+gewichte = "Gewichte/tiefe_gewichte_sigsig_5000_mehrfach_95"
 speicher.load_state_dict(torch.load(gewichte, weights_only=True))
 print('Gewichte geladen.', gewichte)
 
