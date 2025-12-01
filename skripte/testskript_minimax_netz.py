9b7c9e971c9c23596f7f0efc41566ef89bfc1980
ThorstenClausing <163404932+ThorstenClausing@users.noreply.github.com>
2025-12-01 13:03:39 +0100

    Update testskript_minimax_netz.py

--- skripte/testskript_minimax_netz.py
+++ skripte/testskript_minimax_netz.py
@@ -21,7 +21,7 @@ from partieumgebung import Partieumgebung
 anzahl_tests = 100
 speicher = Bewertungsnetz()
 
-gewichte = "Gewichte/tiefe_gewichte_sigsig_trans_4_200"
+gewichte = "Gewichte/tiefe_gewichte_sigsig_trans_4_240"
 speicher.load_state_dict(torch.load(gewichte, weights_only=True))
 print('Gewichte geladen.', gewichte)
 
@@ -29,16 +29,16 @@ spieler_opt = Optimierender_Spieler(speicher)
 tiefe = 6
 spieler_minimax = Alpha_Beta_Spieler(tiefe)
 print("Alpha-Beta-Tiefe ", tiefe, sep='')
-test_schwarz = Partieumgebung(spieler_opt, spieler_minimax)
-#test_weiss = Partieumgebung(spieler_minimax, spieler_opt)
+#test_schwarz = Partieumgebung(spieler_opt, spieler_minimax)
+test_weiss = Partieumgebung(spieler_minimax, spieler_opt)
 
-test_schwarz.testprotokoll_zuruecksetzen()
-for _ in range(anzahl_tests):
-    test_schwarz.test_starten()
-print("Test schwarz:")
-test_schwarz.testprotokoll_drucken()
-#test_weiss.testprotokoll_zuruecksetzen()
+#test_schwarz.testprotokoll_zuruecksetzen()
 #for _ in range(anzahl_tests):
-#    test_weiss.test_starten()
-#print("Test weiß:")
-#test_weiss.testprotokoll_drucken()
+#    test_schwarz.test_starten()
+#print("Test schwarz:")
+#test_schwarz.testprotokoll_drucken()
+test_weiss.testprotokoll_zuruecksetzen()
+for _ in range(anzahl_tests):
+    test_weiss.test_starten()
+print("Test weiß:")
+test_weiss.testprotokoll_drucken()
