5a04e91b83e23aa31de25ac10640460c075e63d2
ThorstenClausing <163404932+ThorstenClausing@users.noreply.github.com>
2025-12-06 16:11:19 +0100

    Create lernskript_weiss.py

---
+++ skripte/lernskript_weiss.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 29 15:09:11 2025
+
+@author: Thorsten
+
+"""
+import sys
+import os
+
+current = os.path.dirname(os.path.realpath(__file__))
+parent = os.path.dirname(current)
+sys.path.append(parent)
+
+from auswertungsumgebung import Ergebnisspeicher
+from spieler import Lernender_Spieler_sigma as Lernender_Spieler
+from spieler import Optimierender_Spieler, Stochastischer_Spieler
+from partieumgebung import Partieumgebung
+
+anzahl_partien = 100000
+anzahl_tests = 1000
+speicher = Ergebnisspeicher(False, True)
+
+spieler_schwarz = Stochastischer_Spieler()
+spieler_weiss = Lernender_Spieler(speicher)
+spieler_opt = Optimierender_Spieler(speicher)
+spieler_stoch = Stochastischer_Spieler()
+partie = Partieumgebung(spieler_schwarz, spieler_weiss, speicher)
+test_weiss = Partieumgebung(spieler_stoch, spieler_opt)
+
+test_weiss.testprotokoll_zuruecksetzen()
+for _ in range(anzahl_tests):
+    test_weiss.test_starten()
+test_weiss.testprotokoll_drucken()
+
+for _ in range(10):
+    for _ in range(anzahl_partien):
+        partie.partie_starten()
+    print('Bewertungen: ', speicher.anzahl_bewertungen())
+    test_weiss.testprotokoll_zuruecksetzen()
+    for _ in range(anzahl_tests):
+        test_weiss.test_starten()
+    test_weiss.testprotokoll_drucken()
+        
+speicher.bewertung_speichern('reversi_weiss')  
