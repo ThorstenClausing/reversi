22d68af60cb161d309834a75f6726e36427a4cfe
ThorstenClausing <163404932+ThorstenClausing@users.noreply.github.com>
2025-12-10 16:14:23 +0100

    Update optimierend_vs_stochastisch.py

--- skripte/optimierend_vs_stochastisch.py
+++ skripte/optimierend_vs_stochastisch.py
@@ -5,12 +5,12 @@ current = os.path.dirname(os.path.realpath(__file__))
 parent = os.path.dirname(current)
 sys.path.append(parent)
 
-from auswertungsumgebung import Ergebnisspeicher
+from bewertungsgeber import Bewertungstabelle
 from spieler import Optimierender_Spieler, Stochastischer_Spieler
 from partieumgebung import Partieumgebung
 
 anzahl_tests = 1000
-speicher = Ergebnisspeicher(True, True)
+speicher = Bewertungstabelle(True, True)
 datei_liste = ['reversi_schwarz_0', 'reversi_schwarz_1']
 speicher.bewertung_laden(datei_liste)
 print('Geladene Bewertungen: ', speicher.anzahl_bewertungen())
@@ -18,14 +18,14 @@ print('Geladene Bewertungen: ', speicher.anzahl_bewertungen())
 spieler_opt = Optimierender_Spieler(speicher)
 spieler_stoch = Stochastischer_Spieler()
 
-#test_schwarz = Partieumgebung(spieler_opt, spieler_stoch, speicher)
+test_schwarz = Partieumgebung(spieler_opt, spieler_stoch, speicher)
 test_weiss = Partieumgebung(spieler_stoch, spieler_opt, speicher)
 
 
-#test_schwarz.testprotokoll_zuruecksetzen()
-#for _ in range(anzahl_tests):
-#    test_schwarz.test_starten()
-#test_schwarz.testprotokoll_drucken()
+test_schwarz.testprotokoll_zuruecksetzen()
+for _ in range(anzahl_tests):
+    test_schwarz.test_starten()
+test_schwarz.testprotokoll_drucken()
 test_weiss.testprotokoll_zuruecksetzen()
 for _ in range(anzahl_tests):
     test_weiss.test_starten()
