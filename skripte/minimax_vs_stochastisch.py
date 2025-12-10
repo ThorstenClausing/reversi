078978cb1b3a5ab45ee74ffba3d2935ee2ffd7e4
ThorstenClausing <163404932+ThorstenClausing@users.noreply.github.com>
2025-12-10 16:13:10 +0100

    Update minimax_vs_stochastisch.py

--- skripte/minimax_vs_stochastisch.py
+++ skripte/minimax_vs_stochastisch.py
@@ -5,12 +5,11 @@ current = os.path.dirname(os.path.realpath(__file__))
 parent = os.path.dirname(current)
 sys.path.append(parent)
 
-#from auswertungsumgebung import Ergebnisspeicher
-from spieler import Alpha_Beta_Spieler
+from spieler import Minimax_Spieler
 from spieler import Stochastischer_Spieler
 from partieumgebung import Partieumgebung
 
-spieler_schwarz = Alpha_Beta_Spieler(4)
+spieler_schwarz = Minimax_Spieler(4)
 spieler_weiss = Stochastischer_Spieler()
 partie = Partieumgebung(spieler_schwarz, spieler_weiss, None)
 partie.testprotokoll_zuruecksetzen()
