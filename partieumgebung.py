ab9e46b9e58b2001599e71437228a45d6187fe3a
ThorstenClausing <163404932+ThorstenClausing@users.noreply.github.com>
2025-12-11 13:32:07 +0100

    Docstring-Kommentare in partieumgebung.py ergänzt

--- partieumgebung.py
+++ partieumgebung.py
@@ -5,13 +5,55 @@ ANZAHL_FELDER = BRETTGROESSE**2
 
  
 class Partieumgebung:
-        
+    """
+    Simuliert und verwaltet Reversi-Partien.
+
+    Diese Klasse koordiniert den Ablauf einer Partie, lässt Spieler Züge wählen,
+    aktualisiert den Spielzustand und ermittelt das Spielergebnis. Sie kann auch
+    Testpartien durchführen und deren Ergebnisse protokollieren.
+
+    Attribute
+    ----------
+    spieler_schwarz : object
+        Das Spielerobjekt, das die Züge für Schwarz wählt.
+    spieler_weiss : object
+        Das Spielerobjekt, das die Züge für Weiß wählt.
+    speicher : object, optional
+        Ein Objekt, das zum Speichern von Spieldaten verwendet wird. 
+        Muss eine Methode `bewertung_aktualisieren(protokoll)` 
+        implementieren. Der Standardwert ist None.
+    testprotokoll : list of (tuple, int, int, int) or None
+        Protokoll für Testpartien. Wird bei der ersten Testpartie initialisiert.
+        Format: `[(summe_punkte_schwarz, summe_punkte_weiss), siege_schwarz, unentschieden, siege_weiss]`.
+        Initial ist es `None`.
+    """    
     def __init__(self,  spieler_schwarz, spieler_weiss, speicher=None):
+      """
+      Initialisiert eine neue Partieumgebung mit den gegebenen Spielern und optionalem Speicher.
+
+      Parameters
+      ----------
+      spieler_schwarz : object
+          Der Spieler, der mit den schwarzen Steinen spielt.
+      spieler_weiss : object
+          Der Spieler, der mit den weißen Steinen spielt.
+      speicher : object, optional
+          Ein Objekt zum Speichern von Bewertungen basierend auf gespielten Partien. 
+          Der Standardwert ist None.
+      """
       self.spieler_schwarz = spieler_schwarz
       self.spieler_weiss = spieler_weiss
       self.speicher = speicher
     
     def partie_starten(self):
+      """
+      Startet eine komplette Partie und führt sie bis zum Ende durch.
+
+      Die Methode initialisiert eine neue Stellung, lässt die Spieler abwechselnd
+      Züge wählen und spielen, bis keine Züge mehr möglich sind. Das Protokoll der Partie
+      (gespielte Züge und Endergebnis) wird, falls vorhanden, an das `speicher`-Objekt
+      übergeben.
+      """
       stellung = Stellung()
       stellung.grundstellung()
       protokoll = []
@@ -40,20 +82,15 @@ class Partieumgebung:
       protokoll.append(ergebnis)
       if self.speicher is not None:
           self.speicher.bewertung_aktualisieren(protokoll)
-      """
-      protokoll.pop()
-      e = '\t'
-      for zug in protokoll:
-        if zug is not None:
-           print(zug[0], end=e)
-        else:
-            print(' pass ', end=e)
-        e = '\n' if e == '\t' else '\t'
-      print(ergebnis)
-      stellung.stellung_anzeigen()
-      """
-    
+          
     def test_starten(self):
+      """
+      Startet eine Testpartie und aktualisiert das interne Testprotokoll.
+
+      Diese Methode funktioniert ähnlich wie `partie_starten`, jedoch wird kein
+      Partieprotokoll an einen externen Speicher übergeben, sondern das Partieergebnis
+      statistisch in `self.testprotokoll` erfasst.
+      """
       stellung = Stellung()
       stellung.grundstellung()
       if self.testprotokoll is None:
@@ -86,33 +123,63 @@ class Partieumgebung:
         self.testprotokoll[3] += 1
 
     def testprotokoll_zuruecksetzen(self):
-        self.testprotokoll = [(0, 0), 0, 0, 0]
-        
+      """
+      Setzt das interne Testprotokoll auf Null zurück.  
+      """
+      self.testprotokoll = [(0, 0), 0, 0, 0]
+              
     def testprotokoll_geben(self):
+      """
+      Gibt das aktuelle Testprotokoll zurück.
+
+      Rückgabe
+      -------
+      list of (tuple, int, int, int) or None
+          Das Testprotokoll im Format:
+          `[(summe_punkte_schwarz, summe_punkte_weiss), siege_schwarz, unentschieden, siege_weiss]`.
+          Gibt `None` zurück, wenn noch keine Testpartie gestartet wurde.
+      """
       return self.testprotokoll
      
     def testprotokoll_drucken(self):
+      """
+      Druckt das aktuelle Testprotokoll auf die Konsole.
+      """
       print(self.testprotokoll)
         
     def __schwarz_am_zug(self, zug_nummer):
-        return zug_nummer % 2 == 1
+      """
+      Überprüft, ob Schwarz (der erste Spieler) am Zug ist.
+
+      Parameters
+      ----------
+      zug_nummer : int
+          Die aktuelle Zugnummer (beginnt bei 1).
+
+      Rückgabe
+      -------
+      bool
+          True, wenn Schwarz am Zug ist (ungerade Zugnummer), False sonst (gerade Zugnummer).
+      """
+      return zug_nummer % 2 == 1
         
     def __ergebnis(self, stellung, zug_nummer):
         """
-        Rückgabewert: Partieergebnis nach WOF-Regeln
-
-        Parameters
+        Berechnet das Endergebnis der Partie basierend auf der finalen Stellung
+        nach WOF-Regeln.
+        
+        Parameter
         ----------
-        stellung : TYPE Stellung
-            DESCRIPTION. Endstellung, für die das Ergebis berechnet werden soll
-        zug_nummer : TYPE Integer
-            DESCRIPTION. Anzahl der bisher ausgeführten Züge (einschließlich passen)
+        stellung : spiellogik.Stellung
+            Die finale Stellung des Spielbretts.
+        zug_nummer : int
+            Die Anzahl der tatsächlich gespielten Züge in der Partie.
 
-        Returns 
+        Rückgabe
         -------
-        TYPE Tupel of Integer
-        DESCRIPTION. Punkte für Schwarz und Weiß nach den WOF-Regeln
-
+        tuple of (int, int)
+            Ein Tupel, das die Punkte für Schwarz und Weiß enthält
+            `(punkte_schwarz, punkte_weiss)`.
         """
         steindifferenz = stellung.sum()
         # Die Steindifferenze besagt, wie viele Steine der Spieler, der am Zug 
@@ -132,4 +199,5 @@ class Partieumgebung:
         # umgedreht werden.
         if zug_nummer % 2 == 1: 
             punktdifferenz = -1*punktdifferenz
+
         return ((ANZAHL_FELDER + punktdifferenz)//2, (ANZAHL_FELDER - punktdifferenz)//2)
